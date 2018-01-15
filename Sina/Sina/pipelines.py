4738760eb019bcd967112156ea5380b7f5803e2f
GuYongtao <873560792@qq.com>
2018-01-15 21:35:47 +0800

    新浪网分类资讯爬虫
    爬取新浪网导航页所有下所有大类、小类、小类里的子链接，以及子链接页面的新闻内容。

---
+++ Sina/Sina/pipelines.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy import signals
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+class SinaPipeline(object):
+    def process_item(self, item, spider):
+        sonUrls = item['sonUrls']
+
+        # 文件名为子链接url中间部分，并将 / 替换为 _，保存为 .txt格式
+        filename = sonUrls[7:-6].replace('/','_')
+        filename += ".txt"
+
+        fp = open(item['subFilename']+'/'+filename, 'w')
+        fp.write(item['content'])
+        fp.close()
+
+        return item
